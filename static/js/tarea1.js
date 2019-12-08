caedfbb1659b7b8951df383f86655b38c2b3ecd1
Philipp Antonius Ferdinand Ulm-Erbach <pulmerba@mail.uni-mannheim.de>
2019-12-08 17:15:16 -0500

    update tarea 1 code

--- static/js/tarea1.js
+++ static/js/tarea1.js
@@ -1,48 +1,94 @@
 /* global vl */
 
+/*Author: Philipp Freiherr von Ulm-Erbach*/
+
 function tarea1(data) {
-    const brush = vl.selectInterval().encodings('x'); // limit selection to x-axis (year) values
-    const click = vl.selectMulti().encodings('x');
+  const brush = vl.selectInterval().encodings('x'); // limit selection to x-axis (year) values
+  const click = vl.selectMulti().encodings('x');
 
-    // ratings scatter plot
-    const base = vl.markArea()
+  // const displayData = 1;
+
+  function getData(display) {
+    if(display == 0){
+      return 'Consumo';
+    } else if (display == 1) {
+      return 'Costo';
+    }
+  }
+
+  function getLabel(display) {
+    if(display == 0){
+      return 'Consumo (KWh)';
+    } else if (display == 1) {
+      return 'Costo (COP)';
+    }
+  }
+
+  function getColor(display) {
+    if(display == 0){
+      return '#EF3939';
+    } else if (display == 1) {
+      return '#EF3939';
+    }
+  }
+
+  // ratings scatter plot
+  const base = vl.markLine()
     .data(data)
     .encode(
-      vl.x().fieldT('Fecha').title(null),
-      vl.y().fieldQ('Consumo').title('Consumo Dia'),
-      vl.tooltip().fieldN('Consumo'),
-      vl.color().value('#2471A3')
+      vl.x()
+        .fieldT('Fecha')
+        .title(null)
+        .axis({format: '%a, %d %b %y', labelAngle: 45}),
+      vl.y()
+        .fieldQ(getData(0))
+        .title(getLabel(0) + ' por Día'),
+      vl.color()
+        .value(getColor(0)),
+      vl.opacity()
+        .value(0.85),
+      vl.tooltip([vl.timeYMD('Fecha'), vl.text(getData(0)).format(',.2f')])
     )
     .transform(
-      vl.filter(brush)
+      vl.filter(click)
     )
-    .width(800)
-    .height(300);
+    .width(850)
+    .height(400);
 
-    // dynamic query histogram
-    const years = vl.markBar({width: 30})
+
+  const numMonths = data.length / 30;
+
+  // dynamic query histogram
+  // const years = vl.markBar({width: (width / numMonths)/1.5})
+  const years = vl.markBar({width: (800 / numMonths)/1.5})
     .data(data)
-    .select(brush)
+    .select(click)
     .encode(
-      vl.x().yearmonth('Fecha').title('Date'),
-      vl.y().sum('Consumo').title('Consumo Mes'),
-      vl.opacity().if(brush, vl.value(1)).value(0.3),
-      vl.color().value('#2471A3')
+      vl.x()
+        .utcYM('Fecha')
+        .title('Date')
+        .axis({title: 'Fecha', labelAngle: 45}),
+      vl.y()
+        .sum(getData(1))
+        .title(getLabel(1) + ' por Mes'),
+      vl.opacity()
+        .if(click, vl.value(0.85)).value(0.3),
+      vl.color()
+        .value(getColor(1)),
+      vl.tooltip([vl.timeYM('Fecha'), vl.text(vl.sum(getData(1))).format(',.0f')])
     )
-    .width(800)
-    .height(50);
+    .width(850)
+    .height(100);
 
-    const textTitle = {color: 'black', fontSize: 22};
-    const textEnergy = {color: 'darkgrey', fontSize: 22};
 
-    const totalEnergia = vl.markText(textEnergy)
+  const totalEnergia = vl.markText()
     .data(data)
     .encode(
-      vl.text().sum('Consumo').format('.2f')
+      vl.text().sum(getData(0)).format(',.2f')
     )
     .transform(
-      vl.filter(brush)
+      vl.filter(click)
     )
 
-    return vl.vconcat(totalEnergia, base, years).spacing(5).toJSON();
+  return vl.vconcat(base, years, totalEnergia).spacing(5).toJSON();
 }
