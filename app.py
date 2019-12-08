0f3d8fbe698ce862eedac3b9712acea5ae7c3994
Philipp Antonius Ferdinand Ulm-Erbach <pulmerba@mail.uni-mannheim.de>
2019-12-08 17:18:57 -0500

    update app.py file for new DBs

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ if __name__ == "__main__":
 
 @app.route('/data')
 def hello():
-    conn = sqlite3.connect('main.db')
+    conn = sqlite3.connect('main1.db')
     cursor = conn.cursor()
-    csvList = list(cursor.execute("select * from ejemplo"))
+    csvList = list(cursor.execute("select * from consumo"))
 
     si = io.StringIO()
     cw = csv.writer(si)
-    cw.writerows([("Fecha", "Hora", "Consumo")] + csvList)
+    # cw.writerows([("Fecha", "Hora", "Consumo")] + csvList)
+    cw.writerows([("CLIENTE", "Fecha", "Consumo", "Costo")] + csvList)
 
     output = make_response(si.getvalue())
     output.headers["Content-Disposition"] = "attachment; filename=export.csv"
@@ -32,11 +33,11 @@ def hello():
 def hello2():
     conn2 = sqlite3.connect('eleDom.db')
     cursor2 = conn2.cursor()
-    csvList2 = list(cursor2.execute("select * from electrodomesticos"))
+    csvList2 = list(cursor2.execute("select * from eleDom"))
 
     si2 = io.StringIO()
     cw2 = csv.writer(si2)
-    cw2.writerows([("electrodomestico", "consumo_kwH", "valor_kwH")] + csvList2)
+    cw2.writerows([("estrato_tarifa", "estrato", "electrodomestico", "consumo_kwH", "valor_kwH", "valorConsumo_Hora")] + csvList2)
 
     output2 = make_response(si2.getvalue())
     output2.headers["Content-Disposition"] = "attachment; filename=export.csv"
